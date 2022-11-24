f4a04d4006772d1846c1281644adb780c698fa91
Colin Zou <zoukaiping@gmail.com>
2022-11-24 14:20:00 +0800

    Added mypy rules

--- src/demo.py
+++ src/demo.py
@@ -1,4 +1,4 @@
-from yapydi import enable_di, bean, BeanRegistry
+from yapydi import enable_di, bean, BeanRegistry, Injected, injected
 
 
 @bean(name="my_bean_name")
@@ -10,11 +10,19 @@ def hello_world():
     registry = BeanRegistry.get_instance()
     str_bean = registry.one_by_name("my_bean_name")
     print(f"Hello {str_bean}!")
+    assert str_bean == "World"
+
+
+@injected
+def test_injection(injected_world: str = Injected[str, "my_bean_name"]):
+    print(f"Hello injected {injected_world}!")
+    assert injected_world == "World"
 
 
 @enable_di
 def main():
     hello_world()
+    test_injection()
 
 
 if __name__ == "__main__":
